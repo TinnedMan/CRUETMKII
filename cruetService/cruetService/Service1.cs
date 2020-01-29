46865f1918c73f81c30c1d7e52e1d40f805cb8d1
Joel tinned <Joelam95@outlook.com>
2020-01-29 23:31:29 +0800

    added libs

--- cruetService/cruetService/Service1.cs
+++ cruetService/cruetService/Service1.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace cruetService
 {
+    
+    
     public partial class Service1 : ServiceBase
     {
+        //public Cookie(string name, string value, string domain, string path, DateTime? expiry);
+        //temp class members
+        string CName, CValue, Cdomain, CPath;
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +26,14 @@ namespace cruetService
 
         protected override void OnStart(string[] args)
         {
+           Cookie MySessionCookie = new Cookie("test","test");
+
+            Console.WriteLine(MySessionCookie.Name);
         }
 
         protected override void OnStop()
         {
+
         }
 
         private void flowLayoutPanel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)

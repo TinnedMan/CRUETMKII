0e8b5cdc5b4a29a928b15693c322aa6270d773c9
Joel tinned <Joelam95@outlook.com>
2020-01-29 22:17:44 +0800

    initial assembly

---
+++ cruetService/cruetService/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cruetService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+        }
+    }
+}
